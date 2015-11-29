98170e87518e7cf0be8dc0f4028be1959372ba70
Levi Shusterman <levi.shusterman@gmail.com>
2015-11-29 17:10:37 -0600

    Got first test case up and running

--- Trie.h
+++ Trie.h
@@ -1,18 +1,22 @@
 #include <vector>
 #include <functional>
+#include <stdexcept>
 
 using std::vector;
 using std::function;
+using std::invalid_argument;
 
 #pragma once
 template <typename key_type, typename data_type>
 class Trie{
     public:
-        bool insert(vector<key_type> key, function<int(key_type)> get_index){
+        bool insert(vector<key_type> key, data_type data, function<int(key_type)> get_index){
         	return true;
         }
         data_type retrieve(vector<key_type>){
-        	return null;
+        	throw invalid_argument("Key not found in trie");
+        	data_type ref;
+        	return ref;
         }
         Trie(int bucketSize){
         	bucket_size = bucketSize;
--- TrieTest.cpp
+++ TrieTest.cpp
@@ -1,21 +1,28 @@
 
 #include "Trie.h"
 #include <iostream>
-
 #include <vector>
+#include <stdexcept>
+
 using std::vector;
 using std::cout;
+using std::invalid_argument;
 
 ///Test insert and retrieval of character keys and integer data
 void test_char_int( ){
 
 	Trie<char,int> test_trie(26);
-	test_trie.insert("alksdfjafds");
+	vector<char> my_string {'a', 'l','k','s' } ;
+	test_trie.insert(my_string, 13, [&]( char a ){ return 1; });
 	
-	if( test_trie.retrieve("alksdfjafds") ){
-		cout << "Test 1 : character key, int data : PASS" ;
-	}else{
-		cout << "Test 1 : character key, int data : FAIL" ;
+	try{
+		if( test_trie.retrieve(my_string) == 13 ){
+			cout << "Test 1 : character key, int data : PASS\n" ;
+		}else{
+			cout << "Test 1 : character key, int data, wrong value : FAIL\n" ;
+		}
+	}catch(const invalid_argument& e){
+		cout << "Test 1 : character key, int data : FAIL \n" ;
 	}
 }
 
